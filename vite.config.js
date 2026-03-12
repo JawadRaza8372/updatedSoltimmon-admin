fc242887666557ae4d00e614b960c0014a2d30d7
Jawad Raza <jawadraza8372@gmail.com>
2026-03-12 10:19:01 +0500

    small change

--- vite.config.js
+++ vite.config.js
@@ -7,54 +7,50 @@ import eslint from "vite-plugin-eslint";
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  plugins: [react(), eslint(), svgrPlugin(), macrosPlugin()],
-  build: {
-    chunkSizeWarningLimit: 1000 * 1000,
-    outDir: "build",
-    rollupOptions: {
-      treeshake: "recommended",
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules/antd")) {
-            return "antd";
-          }
-          if (id.includes("node_modules/@mui")) {
-            return "mui";
-          }
-          if (id.includes("node_modules/recharts")) {
-            return "recharts";
-          }
-          if (id.includes("node_modules/react-select-country-list")) {
-            return "country-list";
-          }
-          if (id.includes("node_modules/country-state-city")) {
-            return "city-list";
-          }
-        },
-      },
-    },
-  },
-  server: {
-    port: 3000,
-  },
-  resolve: {
-    alias: {
-      "@components": "/src/components",
-      "@layout": "/src/layout",
-      "@ui": "/src/ui",
-      "@pages": "/src/pages",
-      "@assets": "/src/assets",
-      "@styles": "/src/styles",
-      "@db": "/src/db",
-      "@hooks": "/src/hooks",
-      "@fonts": "/src/fonts",
-      "@utils": "/src/utils",
-      "@widgets": "/src/widgets",
-      "@contexts": "/src/contexts",
-      "@constants": "/src/constants",
-    },
-  },
-  define: {
-    "process.env": process.env,
-  },
+	plugins: [react(), eslint(), svgrPlugin(), macrosPlugin()],
+	build: {
+		chunkSizeWarningLimit: 1000 * 1000,
+		rollupOptions: {
+			treeshake: "recommended",
+			output: {
+				manualChunks(id) {
+					if (id.includes("node_modules/antd")) {
+						return "antd";
+					}
+					if (id.includes("node_modules/@mui")) {
+						return "mui";
+					}
+					if (id.includes("node_modules/recharts")) {
+						return "recharts";
+					}
+					if (id.includes("node_modules/react-select-country-list")) {
+						return "country-list";
+					}
+					if (id.includes("node_modules/country-state-city")) {
+						return "city-list";
+					}
+				},
+			},
+		},
+	},
+	resolve: {
+		alias: {
+			"@components": "/src/components",
+			"@layout": "/src/layout",
+			"@ui": "/src/ui",
+			"@pages": "/src/pages",
+			"@assets": "/src/assets",
+			"@styles": "/src/styles",
+			"@db": "/src/db",
+			"@hooks": "/src/hooks",
+			"@fonts": "/src/fonts",
+			"@utils": "/src/utils",
+			"@widgets": "/src/widgets",
+			"@contexts": "/src/contexts",
+			"@constants": "/src/constants",
+		},
+	},
+	define: {
+		"process.env": process.env,
+	},
 });
